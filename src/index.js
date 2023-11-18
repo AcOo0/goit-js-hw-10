90ff49665d68a922439727bb190768b9cef9afeb
AcOo0 <acoo.012@gmail.com>
2023-11-18 16:31:09 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import axios from "axios";
+// import Notiflix from 'notiflix';
+// axios.defaults.headers.common["x-api-key"] = "live_9IIzxZ8TCPOsTERuYoSCVZDzM160Q6xVS1M1mfBWaN1ZNYh9N0zckU6T5fL3hn3x";
+
+//  new SlimSelect({
+//   select: '#selectElement'
+// })
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
